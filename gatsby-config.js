47cc4d44d05d035af7daccc3cb5d78175ad7603b
Tyler Barnes <tylerdbarnes@gmail.com>
2020-04-23 11:23:03 -0700

    update config comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
               process.env.NODE_ENV === `development`
                 ? // Lets just pull 50 posts in development to make it easy on ourselves.
                   50
-                : // and we don't actually need more than 5000 in production
+                : // and we don't actually need more than 5000 in production for this particular site
                   5000,
           },
           // this shows how to exclude entire types from the schema
