6d9942c24f42cde5e395bad14bcffe8a98c16b1c
Tyler Barnes <tylerdbarnes@gmail.com>
2020-05-20 17:05:09 -0700

    upgrade to latest packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
       resolve: `gatsby-source-wordpress-experimental`,
       options: {
         url:
-          process.env.WPGRAPHQL_URL ||
-          `https://dev-gatsby-source-wordpress-v4.pantheonsite.io/graphql`,
+          // process.env.WPGRAPHQL_URL ||
+          `https://dev-gatsby-source-wordpress-v4.pantheonsite.io/?graphql`,
         verbose: true,
         schema: {
           queryDepth: 5,
