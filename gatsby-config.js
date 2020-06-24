c470bc0cd32c50cf8dc60969b17fae783ad096a3
Tyler Barnes <tylerdbarnes@gmail.com>
2020-06-24 10:32:22 -0700

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,16 +11,10 @@ module.exports = {
   pathPrefix: process.env.PATH_PREFIX,
   plugins: [
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`,
-      },
-    },
-    {
-      resolve: require.resolve(`../gatsby-theme-wordpress`),
-    },
+    `gatsby-transformer-sharp`,
+    // {
+    //   resolve: require.resolve(`../gatsby-theme-wordpress`),
+    // },
     {
       resolve: require.resolve(`../gatsby-plugin-wordpress/package.json`),
       options: {
@@ -28,75 +22,49 @@ module.exports = {
           process.env.WPGRAPHQL_URL ||
           `https://dev-gatsby-source-wordpress-v4.pantheonsite.io/graphql`,
         verbose: true,
-        schema: {
-          queryDepth: 5,
-          typePrefix: `Wp`,
-          timeout: 30000,
-          // perPage: 10,
-        },
         develop: {
-          nodeUpdateInterval: 3000,
-          hardCacheMediaFiles: false,
-        },
-        production: {
-          hardCacheMediaFiles: false,
-        },
-        html: {
-          useGatsbyImage: false,
+          // this will hard cache images outside of Gatsbys cache in development
+          // that means you wont have to ever download images twice if the
+          // cache is cleared
+          hardCacheMediaFiles: true,
         },
         debug: {
-          // these settings are all the defaults,
-          // remove them if you'd like
           graphql: {
-            showQueryOnError: false,
-            showQueryVarsOnError: true,
-            copyQueryOnError: true,
-            panicOnError: true,
-            // a critical error is a WPGraphQL query that returns an error and no response data. Currently WPGQL will error if we try to access private posts so if this is false it returns a lot of irrelevant errors.
-            onlyReportCriticalErrors: true,
+            // writes internal data sourcing gql queries to
+            // ./WordPress/GraphQL (helpful for debugging)
             writeQueriesToDisk: true,
           },
-          disableCompatibilityCheck: true,
         },
-        auth: {
-          htaccess: {
-            username: null,
-            password: null,
-          },
-        },
-        // fields can be excluded globally.
-        // this example is for wp-graphql-gutenberg.
-        // since we can get block data on the `block` field
-        // we don't need these fields
-        excludeFieldNames: [`blocksJSON`, `saveContent`],
         type: {
           __all: {
-            limit: 50,
+            limit: 10,
+            routes: {
+              single: true,
+              archive: false,
+            },
+          },
+          MediaItem: {
+            routes: {
+              single: false,
+              archive: false,
+            },
+          },
+          User: {
+            routes: {
+              single: false,
+              archive: false,
+            },
           },
           Post: {
-            limit:
-              process.env.NODE_ENV === `development`
-                ? // Lets just pull 50 posts in development to make it easy on ourselves.
-                  50
-                : // and we don't actually need more than 5000 in production for this particular site
-                  5000,
-          },
-          // this shows how to exclude entire types from the schema
-          // these examples are for wp-graphql-gutenberg
-          CoreParagraphBlockAttributes: {
-            exclude: true,
-          },
-          CoreParagraphBlockAttributesV2: {
-            exclude: true,
-          },
-          CorePullquoteBlockAttributes: {
-            exclude: true,
+            limit: process.env.NODE_ENV === `development` ? 50 : 5000,
+            routes: {
+              archive: true,
+            },
           },
         },
       },
     },
     `gatsby-plugin-chakra-ui`,
-    `gatsby-transformer-sharp`,
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
@@ -105,6 +73,5 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-netlify-cache`,
   ],
 }
